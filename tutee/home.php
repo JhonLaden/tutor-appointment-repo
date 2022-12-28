d8e0ef97d9f7ca4086c883a731abd6989c9c0f4b
JhonLaden <116604658+JhonLaden@users.noreply.github.com>
2022-12-28 17:55:50 +0800

    home page

---
+++ tutee/home.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel = "stylesheet" href = "../css/reusable.css">
+    <link rel = "stylesheet" href = "../css/style.css">
+
+    <title>Home</title>
+</head>
+<body>
+    <header>
+        <div class="container container-flex">
+            <div class="logo-quote">
+                <img class = "logo" src="../images/logo.jpg" alt="">
+                <div class="quote">Best tutor in town</div>
+            </div>
+
+            <div class="nav-sign">
+                <i class='icon bx bx-menu'></i>
+                <ul class = "navbar" >
+                    <li><a href="#">HOME</a></li>
+                    <li><a href="#">ABOUT US</a></li>
+                    <li><a href="#">FAQs</a></li>
+                </ul>
+                <button class= "log-in">Login</button>
+                <button class= "sign-up">Sign up</button>
+
+            </div>
+
+        </div>
+        <div class="wall">
+            <div class="container"></div>
+        </div>
+    </header>
+</body>
+</html>
