7d984f08a691a5663538560827aaf2971cfb8b1a
JhonLaden <116604658+JhonLaden@users.noreply.github.com>
2022-12-30 18:35:55 +0800

    update homepage
    updating home page for body

---
+++ end-users/home.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel = "stylesheet" href = "../css/reusable.css">
+    <link rel = "stylesheet" href = "../css/style.css">
+
+    <title>Home</title>
+</head>
+<body>
+    <header>
+        <div class="container container-flex">
+            <div class="logo-quote container-flex">
+                <img class = "logo" src="../images/logo.jpg" alt="">
+                <div class="quote h-text">Best tutor in town</div>
+            </div>
+
+            <div class="nav-sign">
+                <div class = "icon icon-button" >
+                    <i class='bx bx-menu'></i>
+                </div>
+                <ul class = "navbar h-text " >
+                    <li class = "nav-item"><a class = "nav-link active" href="#">HOME</a></li>
+                    <li class = "nav-item"><a class = "nav-link" href="#">FEATURES</a></li>
+                    <li class = "nav-item"><a class = "nav-link" href="#">TUTORS</a></li>
+                    <li class = "nav-item"><a class = "nav-link" href="#">ABOUT US</a></li>
+                    <li class = "nav-item"><a class = "nav-link" href="#">FAQs</a></li>
+
+                    <button class= "log-in">Login</button>
+                    <button class= "sign-up">Sign up</button>
+                </ul>
+             
+
+            </div>
+
+        </div>
+        
+    </header>
+    <main>
+        <div class="wall wall-bg">
+            <div class="container container-wall fluid-height">
+                <div class="wall-body container-wall">
+                        <div class="wall-title">Tutor Appointment</div>
+                        <h1 class="wall-sub-title">Tutoring Services at its best</h1>
+                </div>
+                <img src="../images/wall.png" alt="wall">
+            </div>
+        </div>
+
+        <div class="container-fluid container-main">
+            <div class="container">
+                <div class="pic-description">
+                    <img class = "pic" src="../images/tutor-pic-4.jpg" alt="">
+                    <div class="description">
+                        <p >
+                            Our tutoring services encourage students to achieve success levels
+                            beyond what they may have ever dreamed possible.
+                            All of our students receive excellent virtual or face to face educational
+                            experiences from us, and we work to instill in them a passion 
+                            of lifelong learning as well as a feeling of community
+                            and respect for one another.
+                        </p>
+                    </div>
+                    
+                </div>
+            </div>
+           
+
+
+            <div class="container">
+                <div class="pic-description">
+                    <img class = "pic" src="../images/tutor-pic-1.png" alt="">
+                    <div class="description">
+                        <h2 class="title">Tutors provide</h2>
+                        <p>
+                            By rethinking conventional teaching techniques and developing 
+                            a more dynamic, relevant, and potent model that reflects
+                            contemporary perspectives, we provide a new and modern 
+                            approach to education. We cultivate each student's distinct 
+                            individuality, talent, and self-assurance, preparing them to face
+                            the world with the abilities and self-assurance necessary to
+                            succeed
+                        </p>
+                    
+                    </div>
+                </div>
+            </div>
+           
+
+        </div>
+        
+    </main>
+
+    <script src = "../js/script.js" > </script>
+</body>
+</html>
