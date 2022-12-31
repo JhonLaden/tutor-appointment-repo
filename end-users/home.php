a45ffc12e3b90dc4006c16f61a48a23ee062e7ff
JhonLaden <116604658+JhonLaden@users.noreply.github.com>
2022-12-31 08:44:39 +0800

    completed home page
    complete desing for homepage

--- end-users/home.php
+++ end-users/home.php
@@ -50,49 +50,134 @@
             </div>
         </div>
 
-        <div class="container-fluid container-main">
-            <div class="container">
-                <div class="pic-description">
-                    <img class = "pic" src="../images/tutor-pic-4.jpg" alt="">
-                    <div class="description">
-                        <p >
-                            Our tutoring services encourage students to achieve success levels
-                            beyond what they may have ever dreamed possible.
-                            All of our students receive excellent virtual or face to face educational
-                            experiences from us, and we work to instill in them a passion 
-                            of lifelong learning as well as a feeling of community
-                            and respect for one another.
-                        </p>
-                    </div>
-                    
+
+        <div class="container">
+            <div class="pic-description">
+                <img class = "pic" src="../images/tutor-pic-4.jpg" alt="pic4">
+                <div class="description">
+                    <h2 class="title">Our Tutoring Services</h2>
+                    <p >
+                        Our tutoring services encourage students to achieve success levels
+                        beyond what they may have ever dreamed possible.
+                        All of our students receive excellent virtual or face to face educational
+                        experiences from us, and we work to instill in them a passion 
+                        of lifelong learning as well as a feeling of community
+                        and respect for one another.
+                    </p>
                 </div>
+                
             </div>
-           
-
-
-            <div class="container">
-                <div class="pic-description">
-                    <img class = "pic" src="../images/tutor-pic-1.png" alt="">
-                    <div class="description">
-                        <h2 class="title">Tutors provide</h2>
-                        <p>
-                            By rethinking conventional teaching techniques and developing 
-                            a more dynamic, relevant, and potent model that reflects
-                            contemporary perspectives, we provide a new and modern 
-                            approach to education. We cultivate each student's distinct 
-                            individuality, talent, and self-assurance, preparing them to face
-                            the world with the abilities and self-assurance necessary to
-                            succeed
-                        </p>
-                    
-                    </div>
-                </div>
-            </div>
-           
-
         </div>
         
+        <div class="container">
+            <div class="pic-description reversed">
+                <img class = "pic" src="../images/tutor-pic-1.png" alt="pic1">
+                <div class="description">
+                    <h2 class="title">Tutors provide</h2>
+                    <p>
+                        By rethinking conventional teaching techniques and developing 
+                        a more dynamic, relevant, and potent model that reflects
+                        contemporary perspectives, we provide a new and modern 
+                        approach to education. We cultivate each student's distinct 
+                        individuality, talent, and self-assurance, preparing them to face
+                        the world with the abilities and self-assurance necessary to
+                        succeed.
+                    </p>
+                
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="pic-description">
+                <img class = "pic" src="../images/tutor-pic-2.jpg" alt="pic2">
+                <div class="description">
+                    <h2 class="title">As a student , Why should I hire tutor?</h2>
+                    <p >
+                        Our services are exceptional for their adaptability, method, and accessibility. Students can advance in their learning without interruptions in an effective classroom setting, providing them time to pursue other interests.
+                    </p>
+                </div>
+                
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="pic-description reversed">
+                <img class = "pic" src="../images/tutor-pic-3.jpeg" alt="pic3">
+                <div class="description">
+                    <h2 class="title">As a parent, what do I need to know?</h2>
+                    <p>
+                        The ideal groundwork for future academic endeavors is provided by tutors. Students can make outstanding progress quickly thanks to our cutting-edge teaching methodology.
+                    </p>
+                
+                </div>
+            </div>
+        </div>
+           
+
+        
     </main>
+
+    <footer class = "footer-bg">
+        <div class="info container">
+            <div class="logo-company">
+                <h1 class="footer-logo">LOGO</h1>
+                <p>Locusta Inc.</p>
+            </div>
+
+            <div class="about-contact">
+                <div class="about">
+                    <h2 class = "footer-title">About Us</h2>
+                    <p>
+                        Sample goal of our Website to our customer
+                    </p>
+                </div>
+               
+                <div class="contact">
+                    <h2 class = "footer-title">Contact us</h2>
+                    <ul>
+                        <li>09352006776</li>
+                        <li>jhonladen667@yahoo.com</li>
+                    </ul>
+                </div>
+            </div>
+           
+            <div class="information">
+                <h2 class = "footer-title">Information</h2>
+                <ul>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">More Search</a></li>
+                    <li><a href="#">Blog</a></li>
+                    <li><a href="#">Testimonials</a></li>
+                    <li><a href="#">Events</a></li>
+                </ul>
+            </div>
+
+            <div class="helpful-links">
+                <h2 class="footer-title">Helpful Links</h2>
+                <ul>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Supports</a></li>
+                    <li><a href="#">Terms & Condition</a></li>
+                    <li><a href="#">Privacy Policy</a></li>
+                </ul>
+            </div>
+           
+        </div>
+
+        <div class="platforms">
+            <div class="platforms-logo">
+                <ul>
+                    <li><a href="#"><i class='bx bxl-facebook-square footer-icon'></i></a></li>
+                    <li><a href="#"><i class='bx bxl-instagram-alt footer-icon' ></i></a></li>
+                    <li><a href="#"><i class='bx bxl-twitter footer-icon' ></i></a></li>
+                </ul>
+            </div>
+            <div class="copyright">
+                <p>2018 <i class='bx bx-copyright' ></i> company.Ltd. All Right reserved </p>
+            </div>
+        </div>
+    </footer>
 
     <script src = "../js/script.js" > </script>
 </body>
