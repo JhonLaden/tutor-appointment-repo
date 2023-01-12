be20e99705702ce55b7fbd98265f6f45834f184b
JhonLaden <116604658+JhonLaden@users.noreply.github.com>
2023-01-12 16:12:39 +0800

    update tutors page
    revising tutors page for items

--- end-users/tutors.php
+++ end-users/tutors.php
@@ -14,66 +14,84 @@
         </div>
     </div>
 
+    <form action="../tutors/search-query=?" class = "container search-sort">
+        <div class="search">
+            <label for="search" class = "search-label">Search</label>
+            <input type="text" name = "search" id = "search">
+        </div>
+        <div class="sort">
+            <label for="sort">Sorty by</label>
+                <select name="sort" id="cars">
+                    <option value="name">Name</option>
+                    <option value="stars">Stars</option>
+                    <option value="pay">Payment</option>
+                    <option value="audi">Audi</option>
+                </select>
+        </div>
+    </form>
+
     <div class="card-container container">
+
+    
         <button id = "smooth-btn" class = "prev-btn disabled-btn" ><i class='bx bx-chevron-left'></i></button>
 
         <ul class="list-container" id = "scrollable-element">
             <li class="card">
-                <a href="#">
+                <a href="#"class = "card-link">
                     <img class = "card-img" src="../images/icons/english.png" alt="english">
                     <div class="card-title">English</div>
                 </a>
             </li>
             <li class="card">
-                <a href="#">
+                <a href="#" class = "card-link">
                     <img class = "card-img" src="../images/icons/art.png" alt="art">
                     <div class="card-title">Art</div>
                 </a>
             </li>
             <li class="card">
-                <a href="#">
+                <a href="#" class = "card-link">
                     <img class = "card-img" src="../images/icons/math.png" alt="math">
                     <div class="card-title">Math</div>
                 </a>
             </li>
             <li class="card">
-                <a href="#">
+                <a href="#"class = "card-link">
                     <img class = "card-img" src="../images/icons/history.png" alt="history">
                     <div class="card-title">History</div>
                 </a>
             </li>
             <li class="card">
-                <a href="#">
+                <a href="#"class = "card-link">
                     <img class = "card-img" src="../images/icons/philosophy.png" alt="philosophy">
                     <div class="card-title">Philosophy</div>
                 </a>
             </li>
             <li class="card">
-                <a href="#">
+                <a href="#"class = "card-link">
                     <img class = "card-img" src="../images/icons/technology.png" alt="technology">
                     <div class="card-title">Technology</div>
                 </a>
             </li>
             <li class="card">
-                <a href="#">
+                <a href="#"class = "card-link">
                     <img class = "card-img" src="../images/icons/science.png" alt="science">
                     <div class="card-title">Science</div>
                 </a>
             </li>
             <li class="card">
-                <a href="#">
+                <a href="#"class = "card-link">
                     <img class = "card-img" src="../images/icons/literature.png" alt="literature">
                     <div class="card-title">Literature</div>
                 </a>
             </li>
             <li class="card">
-                <a href="#">
+                <a href="#"class = "card-link">
                     <img class = "card-img" src="../images/icons/geometry.png" alt="geometry">
                     <div class="card-title">Geometry</div>
                 </a>
             </li>
             <li class="card">
-                <a href="#">
+                <a href="#"class = "card-link">
                     <img class = "card-img" src="../images/icons/geography.png" alt="geography">
                     <div class="card-title">Geography</div>
                 </a>
@@ -83,9 +101,13 @@
             
         </ul>
         <button id = "#smooth-btn" class = "next-btn" ><i class='bx bx-chevron-right' ></i></button>
+    </div>
 
+    <div class="container total-services">
+        <p class=" t-color">63,449 services available</p>
 
     </div>
+    
     
     <script src = "../js/tutors.js" > </script>
     
--- js/tutors.js
+++ js/tutors.js
@@ -14,21 +14,35 @@ nextBtn.addEventListener('click', function(){
 
 prevBtn.addEventListener('click', function(){
     scrollableEl.scrollLeft -= 320;
+
+    scrollableEl.addEventListener('scroll', function() {
+        let newScroll = scrollableEl.scrollLeft;
+    });
 });
 
-
+var lastScrollPos = 0;
+var lastScrollHeight = 642.8571166992188;
 // check scroll function is for scrollable effect to check 
 // if the scroll is on the first.
 // if yes, add disabled btn to the icon so that user cant click it
 function checkScroll(newScroll){
     if(newScroll <= 0){
         prevBtn.classList.add('disabled-btn')
-    }else if(prevBtn.classList.contains('disabled-btn')){
-        prevBtn.classList.remove('disabled-btn')
+    }else if(newScroll > lastScrollPos || newScroll < lastScrollPos){
+
+        //in the middle
+        if(prevBtn.classList.contains('disabled-btn')){
+            prevBtn.classList.remove('disabled-btn');
+        }
+        if(nextBtn.classList.contains('disabled-btn')){
+            nextBtn.classList.remove('disabled-btn');
+
+        }
+        lastScrollPos = newScroll;
+        console.log(scrollableEl.scrollLeft);
+    }else if(Math.floor(scrollableEl.scrollLeft + scrollableEl.offsetWidth)+1 >= scrollableEl.scrollWidth){
+        if(!nextBtn.classList.contains('disabled-btn')){
+            nextBtn.classList.add('disabled-btn');
+        }
     }
 }
-
-scrollableEl.addEventListener('scroll', function() {
-    let newScroll = scrollableEl.scrollLeft;
-    console.log(`New scroll position: ${newScroll}px`);
-  });
