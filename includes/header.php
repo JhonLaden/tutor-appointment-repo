dd6717c750e08c8e75c0b9262defac0fa3b66016
JhonLaden <116604658+JhonLaden@users.noreply.github.com>
2023-03-18 00:08:10 +0800

    update logo

--- includes/header.php
+++ includes/header.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel = "icon" href="../images/grid/profile/logo-orig.png" type = "image/x-icon">
     <link rel = "stylesheet" href = "../css/reusable.css">
     <link rel = "stylesheet" href = "../css/style.css">
     <link rel = "stylesheet" href = "../css/login-form.css">
