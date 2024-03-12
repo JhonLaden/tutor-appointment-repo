d09be6c18b79af33f1b9d326698fa2c3c27f9c7f
JhonLaden <116604658+JhonLaden@users.noreply.github.com>
2024-03-12 11:48:37 +0800

    Update tutor_appointment.sql

--- Database/tutor_appointment.sql
+++ Database/tutor_appointment.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 17, 2024 at 02:37 AM
+-- Generation Time: Mar 12, 2024 at 04:45 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -20,6 +20,37 @@ SET time_zone = "+00:00";
 --
 -- Database: `tutor_appointment`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `learner`
+--
+
+CREATE TABLE `learner` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `middlename` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `phone` int(11) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `age` int(11) NOT NULL,
+  `type` varchar(255) NOT NULL DEFAULT 'learner'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `learner`
+--
+
+INSERT INTO `learner` (`id`, `username`, `password`, `firstname`, `middlename`, `lastname`, `phone`, `address`, `age`, `type`) VALUES
+(1, 'qwe', '123', 'Jhon Ladenm', 'bayle ', 'Francisco', 123312, 'Recodo purok 7-A zamboanga city', 14, 'learner'),
+(2, 'asf', 'sfsdf', 'asdfsad', 'sadf', 'sfsfd', 123312, 'Recodo purok 7-A zamboanga city', 20, 'learner'),
+(3, 'asf', 'asdfsd', 'asdfsad', 'sadf', 'sfsfd', 123312, 'Recodo purok 7-A zamboanga city', 20, 'learner'),
+(4, 'hello', 'asdfsd', 'asdffsd', 'sf', 'sfsd', 2147483647, 'Recodo purok 7-A zamboanga city', 21, 'learner'),
+(5, 'hello', '123', 'asdffsd', 'sf', 'sfsd', 2147483647, 'Recodo purok 7-A zamboanga city', 21, 'learner'),
+(6, 'hello', '123', 'asdffsd', 'sf', 'sfsd', 2147483647, 'Recodo purok 7-A zamboanga city', 21, 'learner');
 
 -- --------------------------------------------------------
 
@@ -54,20 +85,60 @@ CREATE TABLE `tutor` (
   `lastname` varchar(255) NOT NULL,
   `phone` int(20) NOT NULL,
   `address` varchar(255) NOT NULL,
-  `age` int(2) NOT NULL
+  `age` int(2) NOT NULL,
+  `type` varchar(255) NOT NULL DEFAULT 'tutor'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tutor`
 --
 
-INSERT INTO `tutor` (`id`, `username`, `password`, `firstname`, `middlename`, `lastname`, `phone`, `address`, `age`) VALUES
-(46, 'saac', '1234', 'saac', 'aaaa', 'ddd', 43, 'asd', 43),
-(47, 'fdsasdfasdfsadf', 'asfdsdfasdf', 'asfdasdf', 'asfasdf', 'asfdasd', 234, 'sdafasfd', 432);
+INSERT INTO `tutor` (`id`, `username`, `password`, `firstname`, `middlename`, `lastname`, `phone`, `address`, `age`, `type`) VALUES
+(103, 'aaa', 'asdfsd', 'asdfsd', 'asdf', 'asdf', 123, 'Recodo purok 7-A zamboanga city', 20, 'tutor'),
+(104, 'aaa', 'asdfdsf', 'asdfsd', 'asdf', 'asdf', 123, 'Recodo purok 7-A zamboanga city', 20, 'tutor'),
+(105, 'aaa', 'sfa', 'asdfsd', 'asdf', 'asdf', 123, 'Recodo purok 7-A zamboanga city', 20, 'tutor'),
+(106, 'aaa', 'asdf', 'asdfsd', 'asdf', 'asdf', 123, 'Recodo purok 7-A zamboanga city', 20, 'tutor'),
+(107, 'aaa', 'asdf', 'asdfsd', 'asdf', 'asdf', 123, 'Recodo purok 7-A zamboanga city', 20, 'tutor'),
+(108, 'aaa', 'sadfsfd', 'asdfsd', 'asdf', 'asdf', 123, 'Recodo purok 7-A zamboanga city', 20, 'tutor'),
+(109, 'asdf', 'asdfsd', 'asdfas', 'asdf', 'asdfa', 2147483647, 'asdf', 20, 'tutor');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tutor_profile`
+--
+
+CREATE TABLE `tutor_profile` (
+  `id` int(11) NOT NULL,
+  `id_fk` int(11) NOT NULL,
+  `subjects` varchar(255) NOT NULL,
+  `experience` varchar(255) NOT NULL,
+  `education` varchar(255) NOT NULL,
+  `services` varchar(255) NOT NULL,
+  `location` varchar(255) NOT NULL,
+  `base` varchar(255) NOT NULL,
+  `fee` varchar(255) NOT NULL,
+  `rating` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tutor_profile`
+--
+
+INSERT INTO `tutor_profile` (`id`, `id_fk`, `subjects`, `experience`, `education`, `services`, `location`, `base`, `fee`, `rating`) VALUES
+(18, 107, '', '', '', '', '', '', '', 0),
+(19, 108, '', '', '', '', '', '', '', 0),
+(20, 109, '', '', '', '', '', '', '', 0);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `learner`
+--
+ALTER TABLE `learner`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `personal_info`
@@ -83,8 +154,21 @@ ALTER TABLE `tutor`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `tutor_profile`
+--
+ALTER TABLE `tutor_profile`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `tutor_id_fk` (`id_fk`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `learner`
+--
+ALTER TABLE `learner`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `personal_info`
@@ -96,7 +180,13 @@ ALTER TABLE `personal_info`
 -- AUTO_INCREMENT for table `tutor`
 --
 ALTER TABLE `tutor`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=110;
+
+--
+-- AUTO_INCREMENT for table `tutor_profile`
+--
+ALTER TABLE `tutor_profile`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- Constraints for dumped tables
@@ -107,6 +197,12 @@ ALTER TABLE `tutor`
 --
 ALTER TABLE `personal_info`
   ADD CONSTRAINT `personal_info_ibfk_1` FOREIGN KEY (`tutor_id`) REFERENCES `tutor` (`id`);
+
+--
+-- Constraints for table `tutor_profile`
+--
+ALTER TABLE `tutor_profile`
+  ADD CONSTRAINT `fk_tutor_profile_tutor_id` FOREIGN KEY (`id_fk`) REFERENCES `tutor` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
